539e61c88d351fe5b63fb0c1ee08eb7115fff247
Henhenz1 <henhenz10@gmail.com>
2020-02-22 12:42:44 -0800

    project submission

--- src/rasterizer.cpp
+++ src/rasterizer.cpp
@@ -22,15 +22,16 @@ void RasterizerImp::fill_pixel(size_t x, size_t y, Color c) {
   // NOTE: You are not required to implement proper supersampling for points and lines
   // It is sufficient to use the same color for all supersamples of a pixel for points and lines (not triangles)
 
-  rgb_framebuffer_target[3 * (y * width + x)    ] = (unsigned char)(c.r * 255);
-  rgb_framebuffer_target[3 * (y * width + x) + 1] = (unsigned char)(c.g * 255);
-  rgb_framebuffer_target[3 * (y * width + x) + 2] = (unsigned char)(c.b * 255);
+        rgb_framebuffer_target[3 * (y * width + x)    ] = (unsigned char)(c.r * 255);
+        rgb_framebuffer_target[3 * (y * width + x) + 1] = (unsigned char)(c.g * 255);
+        rgb_framebuffer_target[3 * (y * width + x) + 2] = (unsigned char)(c.b * 255);
+
 }
 
 // Optional helper function to add a sample to the supersample_buffer
 void RasterizerImp::fill_supersample(size_t x, size_t y, size_t s, Color c) {
   // TODO: Task 2: You may want to implement this function. Hint: our solution uses one line
-
+    supersample_buffer[width * y * sample_rate + x * sample_rate + s] = c;
 };
 
 // Rasterize a point: simple example to help you start familiarizing
@@ -45,7 +46,15 @@ void RasterizerImp::rasterize_point(float x, float y, Color color) {
   if (sx < 0 || sx >= width) return;
   if (sy < 0 || sy >= height) return;
 
-  fill_pixel(sx, sy, color);
+
+  if (sample_rate == 1) {
+      fill_pixel(sx, sy, color);
+  }
+  else {
+      for (int i = 0; i < sample_rate; i++) {
+          fill_supersample(sx, sy, i, color);
+      }
+  }
   return;
 }
 
@@ -67,7 +76,7 @@ void RasterizerImp::rasterize_line(float x0, float y0,
   }
 
   while (floor(pt[0]) <= floor(x1) && abs(pt[1] - y0) <= abs(y1 - y0)) {
-    rasterize_point(pt[0], pt[1], color);
+      rasterize_point(pt[0], pt[1], color);
     pt[0] += dpt[0]; pt[1] += dpt[1];
   }
 }
@@ -78,9 +87,75 @@ void RasterizerImp::rasterize_triangle(float x0, float y0,
                                        float x2, float y2,
                                        Color color) {
   // TODO: Task 1: Implement basic triangle rasterization here, no supersampling
+    // changing order of points to be counterclockwise
+    // https://algs4.cs.princeton.edu/91primitives/
+    if (((x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0)) < 0) {
+        swap(x0, x1);
+        swap(y0, y1);
+    }
+    
+    // declaring xtest and ytest floats
+    float xtest;
+    float ytest;
   
-  // TODO: Task 2: Update to implement super-sampled rasterization
-  
+    // defining bounding box of triangle, dx and dy floats
+    float xmin = min(x0, min(x1, x2));
+    float xmax = max(x0, max(x1, x2));
+    float ymin = min(y0, min(y1, y2));
+    float ymax = max(y0, max(y1, y2));
+
+    float dx0 = x1 - x0;
+    float dx1 = x2 - x1;
+    float dx2 = x0 - x2;
+
+    float dy0 = y1 - y0;
+    float dy1 = y2 - y1;
+    float dy2 = y0 - y2;
+
+    if (sample_rate == 1) {
+        for (float x = floor(xmin); x <= xmax + 1; x++) {
+            for (float y = floor(ymin); y <= ymax + 1; y++) {
+                xtest = x + 0.5;
+                ytest = y + 0.5;
+
+                if (xtest < 0 || xtest >= width) continue;
+                if (ytest < 0 || ytest >= height) continue;
+
+                if ((-(xtest - x0) * dy0 + (ytest - y0) * dx0) >= 0) {
+                    if ((-(xtest - x1) * dy1 + (ytest - y1) * dx1) >= 0) {
+                        if ((-(xtest - x2) * dy2 + (ytest - y2) * dx2) >= 0) {
+                            fill_pixel(xtest, ytest, color);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // TODO: Task 2: Update to implement super-sampled rasterization
+    else {
+        int dim = sqrt(sample_rate);
+        float d0 = (1.0 / dim) / 2; // distance from top left corner of pixel to center of top left subpixel
+        float d = (1.0 / dim);      // distance between centers of subpixels
+        for (float x = floor(xmin); x <= xmax + 1; x++) {
+            for (float y = floor(ymin); y <= ymax + 1; y++) {
+                for (int s = 0; s < sample_rate; s++) {
+                    xtest = x + d0 + d*(s % dim);
+                    ytest = y + d0 + d*floor(s / dim);
+
+                    if (xtest < 0 || xtest >= width) continue;
+                    if (ytest < 0 || ytest >= height) continue;
+
+                    if ((-(xtest - x0) * dy0 + (ytest - y0) * dx0) >= 0) {
+                        if ((-(xtest - x1) * dy1 + (ytest - y1) * dx1) >= 0) {
+                            if ((-(xtest - x2) * dy2 + (ytest - y2) * dx2) >= 0) {
+                                fill_supersample(x, y, s, color);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 
@@ -90,7 +165,83 @@ void RasterizerImp::rasterize_interpolated_color_triangle(float x0, float y0, Co
 {
   // TODO: Task 4: Rasterize the triangle, calculating barycentric coordinates and using them to interpolate vertex colors across the triangle
   // Hint: You can reuse code from rasterize_triangle
+    Color color;
+    float alpha, beta, gamma;
+    
+    if (((x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0)) < 0) {
+        swap(x0, x1);
+        swap(y0, y1);
+    }
 
+    // declaring xtest and ytest floats
+    float xtest;
+    float ytest;
+
+    // defining bounding box of triangle, dx and dy floats
+    float xmin = min(x0, min(x1, x2));
+    float xmax = max(x0, max(x1, x2));
+    float ymin = min(y0, min(y1, y2));
+    float ymax = max(y0, max(y1, y2));
+
+    float dx0 = x1 - x0;
+    float dx1 = x2 - x1;
+    float dx2 = x0 - x2;
+
+    float dy0 = y1 - y0;
+    float dy1 = y2 - y1;
+    float dy2 = y0 - y2;
+
+    if (sample_rate == 1) {
+        for (float x = floor(xmin); x <= xmax + 1; x++) {
+            for (float y = floor(ymin); y <= ymax + 1; y++) {
+                xtest = x + 0.5;
+                ytest = y + 0.5;
+
+                if (xtest < 0 || xtest >= width) continue;
+                if (ytest < 0 || ytest >= height) continue;
+
+                if ((-(xtest - x0) * dy0 + (ytest - y0) * dx0) >= 0) {
+                    if ((-(xtest - x1) * dy1 + (ytest - y1) * dx1) >= 0) {
+                        if ((-(xtest - x2) * dy2 + (ytest - y2) * dx2) >= 0) {
+                            alpha = (-(xtest - x1) * (y2 - y1) + (ytest - y1) * (x2 - x1)) / (-(x0 - x1) * (y2 - y1) + (y0 - y1) * (x2 - x1));
+                            beta = (-(xtest - x2) * (y0 - y2) + (ytest - y2) * (x0 - x2)) / (-(x1 - x2) * (y0 - y2) + (y1 - y2) * (x0 - x2));
+                            gamma = 1 - alpha - beta;
+                            color = alpha * c0 + beta * c1 + gamma * c2;
+                            fill_pixel(xtest, ytest, color);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else {
+        int dim = sqrt(sample_rate);
+        float d0 = (1.0 / dim) / 2; // distance from top left corner of pixel to center of top left subpixel
+        float d = (1.0 / dim);      // distance between centers of subpixels
+        for (float x = floor(xmin); x <= xmax + 1; x++) {
+            for (float y = floor(ymin); y <= ymax + 1; y++) {
+                for (int s = 0; s < sample_rate; s++) {
+                    xtest = x + d0 + d * (s % dim);
+                    ytest = y + d0 + d * floor(s / dim);
+
+                    if (xtest < 0 || xtest >= width) continue;
+                    if (ytest < 0 || ytest >= height) continue;
+
+                    if ((-(xtest - x0) * dy0 + (ytest - y0) * dx0) >= 0) {
+                        if ((-(xtest - x1) * dy1 + (ytest - y1) * dx1) >= 0) {
+                            if ((-(xtest - x2) * dy2 + (ytest - y2) * dx2) >= 0) {
+                                alpha = (-(xtest - x1) * (y2 - y1) + (ytest - y1) * (x2 - x1)) / (-(x0 - x1) * (y2 - y1) + (y0 - y1) * (x2 - x1));
+                                beta = (-(xtest - x2) * (y0 - y2) + (ytest - y2) * (x0 - x2)) / (-(x1 - x2) * (y0 - y2) + (y1 - y2) * (x0 - x2));
+                                gamma = 1 - alpha - beta;
+                                color = alpha * c0 + beta * c1 + gamma * c2;
+                                fill_supersample(x, y, s, color);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 void RasterizerImp::rasterize_textured_triangle(float x0, float y0, float u0, float v0,
@@ -102,13 +253,150 @@ void RasterizerImp::rasterize_textured_triangle(float x0, float y0, float u0, fl
     // TODO: Task 6: Set the correct barycentric differentials in the SampleParams struct.
     // Hint: You can reuse code from rasterize_triangle/rasterize_interpolated_color_triangle
 
+    float alpha, beta, gamma, u, v;
+    int level = 1;
+    Vector2D uv, u1v, uv1;
+    Color c;
+    SampleParams sp;
+
+    if (((x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0)) < 0) {
+        swap(x0, x1);
+        swap(y0, y1);
+        swap(u0, u1);
+        swap(v0, v1);
+    }
+
+    // declaring xtest and ytest floats
+    float xtest;
+    float ytest;
+
+    // defining bounding box of triangle, dx and dy floats
+    float xmin = min(x0, min(x1, x2));
+    float xmax = max(x0, max(x1, x2));
+    float ymin = min(y0, min(y1, y2));
+    float ymax = max(y0, max(y1, y2));
+
+    float dx0 = x1 - x0;
+    float dx1 = x2 - x1;
+    float dx2 = x0 - x2;
+
+    float dy0 = y1 - y0;
+    float dy1 = y2 - y1;
+    float dy2 = y0 - y2;
+
+    if (sample_rate == 1) {
+        for (float x = floor(xmin); x <= xmax + 1; x++) {
+            for (float y = floor(ymin); y <= ymax + 1; y++) {
+                xtest = x + 0.5;
+                ytest = y + 0.5;
+
+                if (xtest < 0 || xtest >= width) continue;
+                if (ytest < 0 || ytest >= height) continue;
+
+                if ((-(xtest - x0) * dy0 + (ytest - y0) * dx0) >= 0) {
+                    if ((-(xtest - x1) * dy1 + (ytest - y1) * dx1) >= 0) {
+                        if ((-(xtest - x2) * dy2 + (ytest - y2) * dx2) >= 0) {
+                            alpha = (-(xtest - x1) * (y2 - y1) + (ytest - y1) * (x2 - x1)) / (-(x0 - x1) * (y2 - y1) + (y0 - y1) * (x2 - x1));
+                            beta = (-(xtest - x2) * (y0 - y2) + (ytest - y2) * (x0 - x2)) / (-(x1 - x2) * (y0 - y2) + (y1 - y2) * (x0 - x2));
+                            gamma = 1 - alpha - beta;
+                            u = alpha * u0 + beta * u1 + gamma * u2;
+                            v = alpha * v0 + beta * v1 + gamma * v2;
+                            uv = Vector2D(u, v);
+
+                            float xp1 = min(xtest + 1, (float)width-1);
+                            float yp1 = min(ytest + 1, (float)height-1);
+
+                            alpha = (-(xp1 - x1) * (y2 - y1) + (ytest - y1) * (x2 - x1)) / (-(x0 - x1) * (y2 - y1) + (y0 - y1) * (x2 - x1));
+                            beta = (-(xp1 - x2) * (y0 - y2) + (ytest - y2) * (x0 - x2)) / (-(x1 - x2) * (y0 - y2) + (y1 - y2) * (x0 - x2));
+                            gamma = 1 - alpha - beta;
+                            u = alpha * u0 + beta * u1 + gamma * u2;
+                            v = alpha * v0 + beta * v1 + gamma * v2;
+                            u1v = Vector2D(u, v);
+
+                            alpha = (-(xtest - x1) * (y2 - y1) + (yp1 - y1) * (x2 - x1)) / (-(x0 - x1) * (y2 - y1) + (y0 - y1) * (x2 - x1));
+                            beta = (-(xtest - x2) * (y0 - y2) + (yp1 - y2) * (x0 - x2)) / (-(x1 - x2) * (y0 - y2) + (y1 - y2) * (x0 - x2));
+                            gamma = 1 - alpha - beta;
+                            u = alpha * u0 + beta * u1 + gamma * u2;
+                            v = alpha * v0 + beta * v1 + gamma * v2;
+                            uv1 = Vector2D(u, v);
+                            
+                            sp.p_uv = uv;
+                            sp.p_dx_uv = u1v;
+                            sp.p_dy_uv = uv1;
+                            sp.lsm = lsm;
+                            sp.psm = psm;
+
+                            c = tex.sample(sp);
+                            fill_pixel(xtest, ytest, c);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else {
+        int dim = sqrt(sample_rate);
+        float d0 = (1.0 / dim) / 2; // distance from top left corner of pixel to center of top left subpixel
+        float d = (1.0 / dim);      // distance between centers of subpixels
+        for (float x = floor(xmin); x <= xmax + 1; x++) {
+            for (float y = floor(ymin); y <= ymax + 1; y++) {
+                for (int s = 0; s < sample_rate; s++) {
+                    xtest = x + d0 + d * (s % dim);
+                    ytest = y + d0 + d * floor(s / dim);
+
+                    if (xtest < 0 || xtest >= width) continue;
+                    if (ytest < 0 || ytest >= height) continue;
+
+                    if ((-(xtest - x0) * dy0 + (ytest - y0) * dx0) >= 0) {
+                        if ((-(xtest - x1) * dy1 + (ytest - y1) * dx1) >= 0) {
+                            if ((-(xtest - x2) * dy2 + (ytest - y2) * dx2) >= 0) {
+                                alpha = (-(xtest - x1) * (y2 - y1) + (ytest - y1) * (x2 - x1)) / (-(x0 - x1) * (y2 - y1) + (y0 - y1) * (x2 - x1));
+                                beta = (-(xtest - x2) * (y0 - y2) + (ytest - y2) * (x0 - x2)) / (-(x1 - x2) * (y0 - y2) + (y1 - y2) * (x0 - x2));
+                                gamma = 1 - alpha - beta;
+                                u = alpha * u0 + beta * u1 + gamma * u2;
+                                v = alpha * v0 + beta * v1 + gamma * v2;
+                                uv = Vector2D(u, v);
+                                
+                                float xp1 = min(xtest + 1, (float)width-1);
+                                float yp1 = min(ytest + 1, (float)height-1);
+
+                                alpha = (-(xp1 - x1) * (y2 - y1) + (ytest - y1) * (x2 - x1)) / (-(x0 - x1) * (y2 - y1) + (y0 - y1) * (x2 - x1));
+                                beta = (-(xp1 - x2) * (y0 - y2) + (ytest - y2) * (x0 - x2)) / (-(x1 - x2) * (y0 - y2) + (y1 - y2) * (x0 - x2));
+                                gamma = 1 - alpha - beta;
+                                u = alpha * u0 + beta * u1 + gamma * u2;
+                                v = alpha * v0 + beta * v1 + gamma * v2;
+                                u1v = Vector2D(u, v);
+
+                                alpha = (-(xtest - x1) * (y2 - y1) + (yp1 - y1) * (x2 - x1)) / (-(x0 - x1) * (y2 - y1) + (y0 - y1) * (x2 - x1));
+                                beta = (-(xtest - x2) * (y0 - y2) + (yp1 - y2) * (x0 - x2)) / (-(x1 - x2) * (y0 - y2) + (y1 - y2) * (x0 - x2));
+                                gamma = 1 - alpha - beta;
+                                u = alpha * u0 + beta * u1 + gamma * u2;
+                                v = alpha * v0 + beta * v1 + gamma * v2;
+                                uv1 = Vector2D(u, v);
+
+                                sp.p_uv = uv;
+                                sp.p_dx_uv = u1v;
+                                sp.p_dy_uv = uv1;
+                                sp.lsm = lsm;
+                                sp.psm = psm;
+
+                                c = tex.sample(sp);
+                                fill_supersample(x, y, s, c);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 void RasterizerImp::set_sample_rate(unsigned int rate) {
   // TODO: Task 2: You may want to update this function for supersampling support
 
   this->sample_rate = rate;
-
+  supersample_buffer.resize(width * height * sample_rate, Color::White);
 }
 
 
@@ -129,7 +417,8 @@ void RasterizerImp::clear_buffers() {
   // Hint: With supersampling, you have an additional buffer to take care of
 
   std::fill(rgb_framebuffer_target, rgb_framebuffer_target + 3 * width * height, 255);
-
+  supersample_buffer.resize(0);
+  supersample_buffer.resize(width * height * sample_rate, Color::White);
 }
 
 
@@ -140,11 +429,49 @@ void RasterizerImp::clear_buffers() {
 //
 void RasterizerImp::resolve_to_framebuffer() {
   // TODO: Task 2: You will likely want to update this function for supersampling support
-
-  
+    if (sample_rate > 1) {
+        float sr;
+        float sg;
+        float sb;
+        Color curr;
+        Color meanc;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                sr = 0;
+                sg = 0;
+                sb = 0;
+                for (int s = 0; s < sample_rate; s++) {
+                    curr = supersample_buffer[width * y *sample_rate + x*sample_rate + s];
+                    sr += curr.r * curr.r;
+                    sg += curr.g * curr.g;
+                    sb += curr.b * curr.b;
+                }
+                sr = sqrt(sr / sample_rate);
+                sg = sqrt(sg / sample_rate);
+                sb = sqrt(sb / sample_rate);
+                meanc = Color(sr, sg, sb);
+                fill_pixel(x, y, meanc);
+            }
+        }
+    }
 }
 
 Rasterizer::~Rasterizer() { }
 
+Vector2D baryouv(float xtest, float ytest, float x0, float y0, float u0, float v0,
+    float x1, float y1, float u1, float v1,
+    float x2, float y2, float u2, float v2) {
+    
+    float alpha, beta, gamma, u, v;
+    Vector2D uv;
+
+    alpha = (-(xtest - x1) * (y2 - y1) + (ytest - y1) * (x2 - x1)) / (-(x0 - x1) * (y2 - y1) + (y0 - y1) * (x2 - x1));
+    beta = (-(xtest - x2) * (y0 - y2) + (ytest - y2) * (x0 - x2)) / (-(x1 - x2) * (y0 - y2) + (y1 - y2) * (x0 - x2));
+    gamma = 1 - alpha - beta;
+    u = alpha * u0 + beta * u1 + gamma * u2;
+    v = alpha * v0 + beta * v1 + gamma * v2;
+    uv = Vector2D(u, v);
+    return uv;
+}
 
 }// CGL
--- src/texture.cpp
+++ src/texture.cpp
@@ -9,12 +9,94 @@ namespace CGL {
 Color Texture::sample(const SampleParams &sp) {
   // TODO: Task 6: Fill this in.
   // return magenta for invalid level
-
-  return Color(1, 0, 1);
+    float level, prop;
+    Color c0, c1;
+    try {
+        switch (sp.lsm) {
+        case 0:
+            switch (sp.psm) {
+            case 0:
+                return sample_nearest(sp.p_uv, 0);
+                break;
+            case 1:
+                return sample_bilinear(sp.p_uv, 0);
+                break;
+            }
+            break;
+        case 1:
+            level = get_level(sp);
+            switch (sp.psm) {
+            case 0:
+                return sample_nearest(sp.p_uv, (int)level);
+                break;
+            case 1:
+                return sample_bilinear(sp.p_uv, (int)level);
+                break;
+            }
+            break;
+        case 2:
+            level = get_level(sp);
+            switch (sp.psm) {
+            case 0:
+                c0 = sample_nearest(sp.p_uv, floor(level));
+                c1 = sample_nearest(sp.p_uv, ceil(level));
+                prop = level - floor(level);
+                return prop * c1 + (1 - prop) * c0;
+                break;
+            case 1:
+                c0 = sample_bilinear(sp.p_uv, floor(level));
+                c1 = sample_bilinear(sp.p_uv, ceil(level));
+                prop = level - floor(level);
+                return prop * c1 + (1 - prop) * c0;
+                break;
+            }
+            break;
+        }
+    }
+    // return magenta for invalid level
+    catch (...) {
+        return Color(1, 0, 1);
+    }
 }
 
 float Texture::get_level(const SampleParams &sp) {
   // TODO: Task 6: Fill this in.
+    Vector2D del_dxuv, del_dyuv;
+    float L, D;
+    switch (sp.lsm) {
+    case 0:
+        return 0;
+        break;
+    case 1:
+        del_dxuv = sp.p_dx_uv - sp.p_uv;
+        del_dxuv.x *= width;
+        del_dxuv.y *= height;
+        
+        del_dyuv = sp.p_dy_uv - sp.p_uv;
+        del_dyuv.x *= width;
+        del_dyuv.y *= height;
+        
+        L = max(sqrt(pow(del_dxuv.x, 2) + pow(del_dxuv.y, 2)), sqrt(pow(del_dyuv.x, 2) + pow(del_dyuv.y, 2)));
+        D = round(log2(L));
+        return min((float)(mipmap.size() - 1), max((float)0, D));
+
+        break;
+    case 2:
+        del_dxuv = sp.p_dx_uv - sp.p_uv;
+        del_dxuv.x *= width;
+        del_dxuv.y *= height;
+        
+        del_dyuv = sp.p_dy_uv - sp.p_uv;
+        del_dyuv.x *= width;
+        del_dyuv.y *= height;
+        
+        L = max(sqrt(pow(del_dxuv.x, 2) + pow(del_dxuv.y, 2)), sqrt(pow(del_dyuv.x, 2) + pow(del_dyuv.y, 2)));        
+        D = log2(L);
+        
+        return min((float)(mipmap.size()-1), max((float)0, D));
+
+        break;
+    }
   return 0;
 }
 
@@ -24,14 +106,80 @@ Color MipLevel::get_texel(int tx, int ty) {
 
 Color Texture::sample_nearest(Vector2D uv, int level) {
   // TODO: Task 5: Fill this in.
+    try {
+        int tx = (int) round(mipmap[level].width * uv.x);
+        int ty = (int) round(mipmap[level].height * uv.y);
+        if (tx >= mipmap[level].width) {
+            tx -= 1;
+        }
+        if (ty >= mipmap[level].height) {
+            ty -= 1;
+        }
+        return mipmap[level].get_texel(tx, ty);
+    } 
   // return magenta for invalid level
-  return Color(1, 0, 1);
+    catch (...) {
+        return Color(1, 0, 1);
+    }
 }
 
 Color Texture::sample_bilinear(Vector2D uv, int level) {
   // TODO: Task 5: Fill this in.
-  // return magenta for invalid level
-  return Color(1, 0, 1);
+    try {
+        Vector2D xy = Vector2D(uv.x * mipmap[level].width, uv.y*mipmap[level].height);
+        float u0 = floor(xy.x);
+        float v0 = floor(xy.y);
+        float u1 = ceil(xy.x);
+        float v1 = ceil(xy.y);
+
+        if (u0 < 0) {
+            u0 += 1;
+        }
+        else if (u1 >= mipmap[level].width) {
+            u1 -= 1;
+        }
+
+        if (v0 < 0) {
+            v0 += 1;
+        }
+        else if (v1 >= mipmap[level].height) {
+            v1 -= 1;
+        }
+        
+        float s = xy.x - u0;
+        float t = xy.y - v0;
+
+        Color u00 = mipmap[level].get_texel(u0, v0);
+        Color u10 = mipmap[level].get_texel(u1, v0);
+        Color u01 = mipmap[level].get_texel(u0, v1);
+        Color u11 = mipmap[level].get_texel(u1, v1);
+
+        // calculating individual RGB values for first lerp
+        float r0 = u00.r + (u10.r - u00.r) * s;
+        float g0 = u00.g + (u10.g - u00.g) * s;
+        float b0 = u00.b + (u10.b - u00.b) * s;
+        Color c0 = Color(r0, g0, b0);
+        // second lerp
+        float r1 = u01.r + (u11.r - u01.r) * s;
+        float g1 = u01.g + (u11.g - u01.g) * s;
+        float b1 = u01.b + (u11.b - u01.b) * s;
+        Color c1 = Color(r1, g1, b1);
+        // last lerp
+        float r2 = c0.r + (c1.r - c0.r) * t;
+        float g2 = c0.g + (c1.g - c0.g) * t;
+        float b2 = c0.b + (c1.b - c0.b) * t;
+        Color c2 = Color(r2, g2, b2);
+        return c2;
+    }
+    // return magenta for invalid level
+    catch (...) {
+        return Color(1, 0, 1);
+    }
+}
+
+Vector2D uvtoxy(float u, float v, float mmw, float mmh) {
+    Vector2D* xy = new Vector2D(mmw * u, mmh * v);
+    return *xy;
 }
 
 /****************************************************************************/
